eb6c30c720e30df8462a1bbb620df2d359735cfa
MxNl <maxnoelscher@posteo.de>
2020-04-13 23:28:53 +0200

    comparison with corona weeks

---
+++ functions/cus_fun_scrape_measurement_begin_f.R
@@ -0,0 +1,6 @@
+cus_fun_scrape_measurement_begin <- function(url) {
+  url %>% 
+    read_html() %>% 
+    html_node(xpath = "/html/body/div[3]/div/div/div/div[3]/div/div[2]/div[2]/div[2]/div/div[2]/dl/dd[3]") %>% 
+    html_text()
+}
