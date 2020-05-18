6676683333ea50591b65dd05ce3dc777733e60ea
MxNl <maxnoelscher@posteo.de>
2020-05-18 15:19:39 +0200

    initial commit of custom functions

--- functions/cus_fun_scrape_measurement_begin_f.R
+++ functions/cus_fun_scrape_measurement_begin_f.R
@@ -4,3 +4,4 @@ cus_fun_scrape_measurement_begin <- function(url) {
     html_node(xpath = "/html/body/div[3]/div/div/div/div[3]/div/div[2]/div[2]/div[2]/div/div[2]/dl/dd[3]") %>% 
     html_text()
 }
+
